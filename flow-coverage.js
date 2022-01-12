77d42b6e2de9d8ab3c2badc72f28c2641e91515b
Jared Forsyth <jared@jaredforsyth.com>
2022-01-12 14:13:20 -0600

    wip

--- flow-coverage.js
+++ flow-coverage.js
@@ -1,41 +1,10 @@
-#!/usr/bin/env node
 // @flow
 
-/**
- * This action runs `flow` and lints against uncovered expressions.
- *
- * It uses `send-report.js` to support both running locally (reporting to
- * stdout) and under Github Actions (adding annotations to files in the GitHub
- * UI).
- */
-
-// $FlowFixMe: shhhhh
-require('@babel/register'); // flow-uncovered-line
-
-const sendReport = require('actions-utils/send-report');
-const getBaseRef = require('actions-utils/get-base-ref');
-const {cannedGithubErrorMessage} = require('actions-utils/get-base-ref');
-const gitChangedFiles = require('actions-utils/git-changed-files');
 const fs = require('fs');
-
+const sendReport = require('actions-utils/send-report');
 const checkFile = require('./flow-coverage-linter');
 
-async function run(flowBin) {
-    const subtitle = process.env['INPUT_CHECK-RUN-SUBTITLE'];
-    const workingDirectory = process.env['INPUT_CUSTOM-WORKING-DIRECTORY'];
-
-    const baseRef = await getBaseRef();
-    if (!baseRef) {
-        console.log('Unable to determine base ref');
-        console.error(cannedGithubErrorMessage());
-        return;
-    }
-    const files = await gitChangedFiles(baseRef, workingDirectory || '.');
-    const jsFiles = files.filter(file => file.endsWith('.js'));
-    if (!jsFiles.length) {
-        console.log('No changed files');
-        return;
-    }
+export async function runFlowCoverage(subtitle: ?string, flowBin: string, jsFiles: Array<string>) {
     const allAnnotations = [];
     for (const file of jsFiles) {
         const annotations = await checkFile(flowBin, file);
@@ -44,7 +13,7 @@ async function run(flowBin) {
     await sendReport(`Flow Coverage${subtitle ? ' - ' + subtitle : ''}`, allAnnotations);
 }
 
-const getFlowBin = () /*:string*/ => {
+export const getFlowBin = () /*:string*/ => {
     if (process.env['INPUT_FLOW-BIN']) {
         return process.env['INPUT_FLOW-BIN'];
     }
@@ -56,9 +25,3 @@ const getFlowBin = () /*:string*/ => {
     process.exit(1);
     throw new Error();
 };
-
-// flow-next-uncovered-line
-run(getFlowBin()).catch(err => {
-    console.error(err); // flow-uncovered-line
-    process.exit(1);
-});
